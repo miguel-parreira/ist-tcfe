6363732d259fcbd826c951c9dd5dc6ef44be7f00
Megum <joao.c.g.rebelo@tecnico.ulisboa.pt>
2021-04-01 12:27:02 +0100

    Mudei o python para escrever ficheiro(3h perdidas) e ainda estou a tentar ler no octave

--- t2/t2_datagen.py
+++ t2/t2_datagen.py
@@ -7,27 +7,35 @@
 
 import math
 import random
+import sys
 
 class Var:
     def __init__(self, name, val, tol):
         self.name = name;
         self.val = val + random.random()*(val*float(tol)/100)
-        
+
     def printVar(self):
         print self.name, "=", self.val, "\n",
-        
+
+    def printVar0(self):
+        print self.name, "=", self.val, "\n",
+
 class Prob:
     def __init__(self, number, weight):
         self.number = number
         self.varList = []
         self.weight = weight
-        
+
     def printProb(self):
         print "Values: ",
         for i in range(len(self.varList)):
             self.varList[i].printVar()
         print "\n\n"
-            
+
+    def printProb0(self):
+        for i in range(len(self.varList)):
+            self.varList[i].printVar0()
+
     def addVar(self, var):
         self.varList.append(var)
 
@@ -36,13 +44,13 @@ class DataSet:
         random.seed(number)
         self.probList = []
 
-        #ENTER ERROR TOLERANCE IN % FOR ANSWERS 
-        self.errtol = 1 
-        
+        #ENTER ERROR TOLERANCE IN % FOR ANSWERS
+        self.errtol = 1
+
         #
         #ADD PROBLEMS AND THEIR VARIABLES HERE
         #
-        
+
         self.prob = Prob(1, 1) #problem number and weight in mark
         self.prob.addVar(Var("R1", 1, 5)) #var name, value, variation in %
         self.prob.addVar(Var("R2", 2, 5))
@@ -57,12 +65,17 @@ class DataSet:
         self.prob.addVar(Var("Kd", 8, 5))
 
         self.probList.append(self.prob)
-    
+
     def printDataSet(self):
         nprobs = len(self.probList)
         print "Units for the values: V, mA, kOhm, mS and uF\n\n"
         for i in range(nprobs):
             self.probList[i].printProb()
+
+    def printDataTxt(self):
+        nprobs = len(self.probList)
+        for i in range(nprobs):
+            self.probList[i].printProb0()
 
 def main():
     #init test
@@ -73,7 +86,12 @@ def main():
 
     #print test
     dataset.printDataSet()
-    
+
+    original_stdout = sys.stdout # Save a reference to the original standard output
+
+    with open('data.txt', 'w') as f:
+        sys.stdout = f # Change the standard output to the file we created.
+        dataset.printDataTxt()
+        sys.stdout = original_stdout # Reset the standard output to its original value
+
 if __name__ == "__main__": main()
-
-
