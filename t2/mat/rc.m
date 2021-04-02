ec8b79813caaf5bb02a0a1ad8872affa10283b7d
Megum <joao.c.g.rebelo@tecnico.ulisboa.pt>
2021-04-02 19:10:02 +0100

    T6 - falta fazer graficos

--- t2/mat/rc.m
+++ t2/mat/rc.m
@@ -85,7 +85,7 @@ v6_n = v2(6)*exp(-t/Tau);
 v8_n = v2(8)*exp(-t/Tau);
 
 f = figure ();
-plot (t*1000, v6_n, "g");
+plot (t*1000, v6_n, "r");
 
 xlabel ("t[ms]");
 ylabel ("v6_nu [V]");
@@ -117,7 +117,7 @@ Phase6 = angle(v3(6))
 v6_f = Gain6*sin(w*t+Phase6);
 
 f2 = figure ();
-plot (t*1000, v6_f, "g");
+plot (t*1000, v6_f, "r");
 
 xlabel ("t[ms]");
 ylabel ("v6_f(t) [V]");
@@ -142,7 +142,7 @@ vs(phase1) = Vs;
 vs(phase2) = sin(w*t(phase2));
 
 f3 = figure ();
-plot (t*1000, V6, "g");
+plot (t*1000, V6, "r");
 hold on
 plot (t*1000, vs, "m");
 
@@ -151,6 +151,62 @@ ylabel ("v6(t), vs(t) [V]");
 print (f3, "v6.eps", "-depsc");
 
 printf("\n\n---->Ponto 6 - Estudo frequência:\n");
-w=0:1:1e6;
 
-Vc = 1/(1+j*w*Tau);
+frq = logspace(-1, 6, 200);
+
+function out = phaser6 (in)
+  TxtFile = fopen("data.txt", "r");
+  data = dlmread (TxtFile, '\n');
+  fclose(TxtFile);
+
+  R1 = data(1);
+  R2 = data(2);
+  R3 = data(3);
+  R4 = data(4);
+  R5 = data(5);
+  R6 = data(6);
+  R7 = data(7);
+  Vs = data(8);
+  C = data(9);
+  Kb = data(10);
+  Kd = data(11);
+
+  G1=1/R1;
+  G2=1/R2;
+  G3=1/R3;
+  G4=1/R4;
+  G5=1/R5;
+  G6=1/R6;
+  G7=1/R7;
+
+  out = 0;
+  w = 2*pi*in;
+  Zc = 1/(j*C*w);
+  Gc = 1/Zc;
+
+  A1 = [1, 0, 0, 0, 0, 0, 0, 0];
+  A2 = [-G1, G1+G2+G3, -G2, 0, -G3, 0, 0, 0];
+  A3 = [0, -Kb-G2, G2, 0, Kb, 0, 0, 0];
+  A4 = [0, 0, 0, 1, 0, 0, 0, 0];
+  A5 = [0, -G3, 0, -G4, G4+G3+G5, -G5+Gc, -G7, G7-Gc];
+  A6 = [0 , -Kb, 0, 0, G5+Kb, -G5+Gc, 0, -Gc];
+  A7 = [0, 0, 0, G6, 0, 0, -G6-G7, G7];
+  A8 = [0, 0, 0, -Kd*G6, 1, 0, Kd*G6, -1];
+
+  A=[A1; A2; A3; A4; A5; A6; A7; A8];
+  b = [1; 0; 0; 0; 0; 0; 0; 0];
+
+  v3=A\b;
+  out = v3(6);
+endfunction
+
+MAG6 = abs(phaser6)
+PHASE6 = angle(phaser6)
+
+
+f4 = figure ();
+plot (frq, MAG6, "r");
+
+xlabel ("log(f) (Hz)");
+ylabel ("v6(t) [dB]");
+print (f4, "MAG_v6.eps", "-depsc");
