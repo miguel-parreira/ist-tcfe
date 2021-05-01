52c6b4cf1967c7451aad12fb2d78f55dea237751
bernardofrazao2001 <bernardo-frazao@hotmail.com>
2021-05-02 00:16:08 +0100

    TENTAMOS

--- t3/mat/rc.m
+++ t3/mat/rc.m
@@ -1,74 +1,30 @@
 close all
 clear all
+pkg load control
+format long
+function xdot = f (x, t)
+  a=pi;
+  R=100000;
+  C=100 * 1e-6;
+  Is=1e-14;
+  eta=1;
+  Vt=1e-3;
+  dV1=-2*a*50*23*cos(2*a*50*t);
+  V1=23*sin(2*a*50*t);
 
-%%EXAMPLE SYMBOLIC COMPUTATIONS
+  xdot=(1/(R*C))*(-x-R*Is*(exp(x/(7*eta*Vt))-1)+ R*C*dV1+V1);
 
-pkg load symbolic
+endfunction
 
-syms t
-syms R
-syms C
-syms vi(t)
-syms vo(t)
-syms i(t)
+lsode_options("integration method", "non-stiff")
 
-i(t)=C*diff(vo,t)
-
-printf("\n\nKVL equation:\n");
-
-vi(t) = R*i(t)+vo(t)
-
-syms vo_n(t) %natural solution
-syms vo_f(t) %forced solution
-
-printf("\n\nSolution is of the form");
-
-v(t) = vo_n(t) + vo_f(t)
-
-printf("\n\nNatural solution:\n");
-syms A
-syms wn
-
-vi(t) = 0 %no excitation
-i_n(t) = C*diff(vo_n, t)
-
-
-printf("\n\n Natural solution is of the form");
-vo_n(t) = A*exp(wn*t)
-
-R*i_n(t)+vo_n(t) == 0
-
-R*C*wn*vo_n(t)+vo_n(t) == 0
-
-R*C*wn+1==0
-
-solve(ans, wn)
-
-
-%%EXAMPLE NUMERIC COMPUTATIONS
-
-R=1e3 %Ohm
-C=100e-9 %F
-
-f = 1000 %Hz
-w = 2*pi*f; %rad/s
-
-%time axis: 0 to 10ms with 1us steps
-t=0:1e-6:10e-3; %s
-
-Zc = 1/(j*w*C)
-Cgain = Zc/(R+Zc)
-Gain = abs(Cgain)
-Phase = angle(Cgain)
-
-vi = 1*cos(w*t);
-vo = Gain*cos(w*t+Phase);
+f(1,2)
+x0=0;
+t=linspace(0,0.6,1000);
+y=lsode("f",x0,t)
 
 hf = figure ();
-plot (t*1000, vi, "g");
-hold on;
-plot (t*1000, vo, "b");
-
+plot (t, y, "g");
 xlabel ("t[ms]");
 ylabel ("vi(t), vo(t) [V]");
 print (hf, "forced.eps", "-depsc");
